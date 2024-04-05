f4e4175bbee69047fc149bf8929528c9144fc114
Isabel Solano <60373842+MaIsabelSolano@users.noreply.github.com>
2024-04-05 15:04:02 -0600

    Feature: Movimiento de Kathy Mini

--- Assets/KathyMovement.cs
+++ Assets/KathyMovement.cs
@@ -10,6 +10,10 @@ public class KathyMovement : MonoBehaviour
     private Rigidbody2D rigidbody2D_;
     [SerializeField] float speed_ = 5f; // Movement speed
     Vector2 movement;
+    
+    // animación
+    public Animator animator;
+
     void Start()
     {
         rigidbody2D_ = GetComponent<Rigidbody2D>();
@@ -20,6 +24,16 @@ public class KathyMovement : MonoBehaviour
         // Get input from the player
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 ||
+            Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1) {
+                animator.SetFloat("LastHorizontal", Input.GetAxisRaw("Horizontal"));
+                animator.SetFloat("LastVertical", Input.GetAxisRaw("Vertical"));
+            }
 
     }
 
